856e39c955d238cb3246549177f9e35af268a582
maeeive <anonimobeastw@gmail.com>
2026-05-02 00:10:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ setTimeout(() => {
 
     #coquette-header {
         position: fixed;
-        top: 70px; /* Bem mais alto conforme pedido */
+        top: 100px; /* Bem mais alto conforme pedido */
         left: 50%;
         transform: translateX(-50%);
         z-index: 9999;
