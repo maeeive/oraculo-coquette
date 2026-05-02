2d29a6971e18acd8c12016077db1459941a4d5a0
maeeive <anonimobeastw@gmail.com>
2026-05-02 00:48:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ jQuery(async () => {
     frame.style.top = "0";
     frame.style.left = "0";
     frame.style.width = "100vw";
-    frame.style.height = "calc(100vh - 83px)"; // 🔥 FIX AQUI
+    frame.style.height = "calc(100vh - 75px)"; // 🔥 FIX AQUI
     frame.style.pointerEvents = "none";
     frame.style.zIndex = "9995";
 
